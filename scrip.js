aa017ee3c5fa1a9360abb6dfd27cea5ea74fb4f5
ugvarodata-create <ugvarodata@gmail.com>
2026-08-14 21:01:15 +0400

    Add files via upload

---
+++ scrip.js
@@ -0,0 +1,16 @@
+function showMessage() {
+    document.getElementById("message").innerText =
+        "🔥 საიტი მუშაობს!";
+}
+
+function openGame() {
+    alert("🎮 Cube Jump მალე დაემატება!");
+}
+
+function showAI() {
+    alert("🤖 AI პროექტი მალე დაემატება!");
+}
+
+function calculator() {
+    alert("🧮 კალკულატორი მალე დაემატება!");
+}
